00b2b90e9ea5879fd2fc0c3fcc9f082b3202322b
iabdullahbayram <abdullahbayram1997@hotmail.com>
2024-02-15 19:35:38 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            int s1,s2,s3;
+            s1 = rd.Next(1, 10);
+            s2 = rd.Next(1, 10);
+            s3 = rd.Next(1, 10);
+
+            label1.Text = s1.ToString();
+            label2.Text = s2.ToString();
+            label3.Text = s3.ToString();
+
+            if (textBox1.Text == label1.Text)
+            {
+                textBox1.BackColor = Color.Green;
+
+            }
+            else
+            {
+                textBox1.BackColor = Color.Red;
+            }
+            if (textBox2.Text == label2.Text)
+            {
+                textBox2.BackColor = Color.Green;
+
+            }
+            else
+            {
+                textBox2.BackColor = Color.Red;
+            }
+            if (textBox3.Text == label3.Text)
+            {
+                textBox3.BackColor = Color.Green;
+
+            }
+            else
+            {
+                textBox3.BackColor = Color.Red;
+            }
+        }
+    }
+}
